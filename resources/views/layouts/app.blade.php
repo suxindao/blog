dde4405b58cc337ccefdf29714ef14b84aa01ff1
suxindao <suxindao@126.com>
2016-05-08 22:46:25 +0800

    move .js into head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,73 +16,73 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/jquery.fullpage.min.css" rel="stylesheet">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    <script src="/js/require.js" data-main="{{ elixir('js/config.js') }}"></script>
+    {{--<script src="{{ elixir('js/app.js') }}"></script>--}}
+  <style>
+    body {
+      font-family: 'Lato';
+    }
 
-    <style>
-      body {
-        font-family: 'Lato';
-      }
+    .fa-btn {
+      margin-right: 6px;
+    }
+  </style>
+</head>
+<body id="app-layout">
+  <nav class="navbar navbar-inverse navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
 
-      .fa-btn {
-        margin-right: 6px;
-      }
-    </style>
-  </head>
-  <body id="app-layout">
-    <nav class="navbar navbar-inverse navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
+        <!-- Collapsed Hamburger -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+          <span class="sr-only">Toggle Navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
-          <!-- Collapsed Hamburger -->
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-            <span class="sr-only">Toggle Navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <!-- Branding Image -->
-          <a class="navbar-brand" href="{{ url('/') }}">
-            Laravel
-          </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-          <!-- Left Side Of Navbar -->
-          <ul class="nav navbar-nav">
-            <li><a href="{{ url('/home') }}">Home</a></li>
-            <li><a href="{{ url('/fullpage') }}">全屏</a></li>
-          </ul>
-
-          <!-- Right Side Of Navbar -->
-          <ul class="nav navbar-nav navbar-right">
-            <!-- Authentication Links -->
-            @if (Auth::guest())
-            <li><a href="{{ url('/login') }}">Login</a></li>
-            <li><a href="{{ url('/register') }}">Register</a></li>
-            @else
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                {{ Auth::user()->name }} <span class="caret"></span>
-              </a>
-
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-              </ul>
-            </li>
-            @endif
-          </ul>
-        </div>
+        <!-- Branding Image -->
+        <a class="navbar-brand" href="{{ url('/') }}">
+          Laravel
+        </a>
       </div>
-    </nav>
 
-    @yield('content')
+      <div class="collapse navbar-collapse" id="app-navbar-collapse">
+        <!-- Left Side Of Navbar -->
+        <ul class="nav navbar-nav">
+          <li><a href="{{ url('/home') }}">Home</a></li>
+          <li><a href="{{ url('/fullpage') }}">全屏</a></li>
+        </ul>
 
-  </body>
-  <!-- JavaScripts -->
-  <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
-  <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
-  <!--<script src="js/jquery.min.js"></script>-->
-  <!--<script src="js/bootstrap.min.js"></script>-->
-  <script src="/js/require.js" data-main="{{ elixir('js/config.js') }}"></script>
-  {{--<script src="{{ elixir('js/app.js') }}"></script>--}}
+        <!-- Right Side Of Navbar -->
+        <ul class="nav navbar-nav navbar-right">
+          <!-- Authentication Links -->
+          @if (Auth::guest())
+          <li><a href="{{ url('/login') }}">Login</a></li>
+          <li><a href="{{ url('/register') }}">Register</a></li>
+          @else
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+              {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+            </ul>
+          </li>
+          @endif
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  @yield('content')
+
+</body>
+<!-- JavaScripts -->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+<!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
+<!--<script src="js/jquery.min.js"></script>-->
+<!--<script src="js/bootstrap.min.js"></script>-->
+
 </html>
